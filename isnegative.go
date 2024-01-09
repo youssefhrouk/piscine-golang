9c6e31751dd504585fe156c3914a388557fd4295
yhrouk <yhrouk@gmail.com>
2024-01-09 12:27:04 +0000

    hello

--- isnegative.go
+++ isnegative.go
@@ -1,4 +1,4 @@
-package main
+package Isnegative
 
 import "github.com/01-edu/z01"
 
@@ -10,10 +10,4 @@ func IsNegative(nb int) {
 		z01.PrintRune('F')
 		z01.PrintRune('\n')
 	}
-}
-
-func main() {
-	IsNegative(1)
-	IsNegative(0)
-	IsNegative(-1)
 }

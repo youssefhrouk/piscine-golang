91578f9839d78f458dca135e0ead406164cdb1a0
yhrouk <yhrouk@gmail.com>
2024-01-10 10:42:23 +0000

    printcomb

---
+++ printcomb2.go
@@ -0,0 +1,28 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func PrintComb2() {
+	for i := '0'; i <= '9'; i++ {
+		for j := '0'; j <= '8'; j++ {
+			for k := i; k <= '9'; k++ {
+				for z := j + 1; z <= '9'; z++ {
+					z01.PrintRune(i)
+					z01.PrintRune(j)
+					z01.PrintRune(' ')
+					z01.PrintRune(k)
+					z01.PrintRune(z)
+					if i < '9' || j < '8' || k < '9' || z < '9' {
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+					}
+				}
+			}
+		}
+	}
+	z01.PrintRune('\n')
+}
+
+func main() {
+	PrintComb2()
+}

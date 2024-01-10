119efcd91d820527e55a92e2b9b5632b21968fe3
yhrouk <yhrouk@gmail.com>
2024-01-10 16:47:39 +0000

    echo

--- printcomb2.go
+++ printcomb2.go
@@ -1,4 +1,4 @@
-package piscine
+package main
 
 import "github.com/01-edu/z01"
 
@@ -7,7 +7,7 @@ func PrintComb2() {
 		for b := '0'; b <= '9'; b++ {
 			for c := '0'; c <= '9'; c++ {
 				for d := '0'; d <= '9'; d++ {
-					if a < c || (a == c && d > b) {
+					if a == c && d > b {
 						z01.PrintRune(a)
 						z01.PrintRune(b)
 						z01.PrintRune(' ')
@@ -23,4 +23,8 @@ func PrintComb2() {
 		}
 	}
 	z01.PrintRune('\n')
+}
+
+func main() {
+	PrintComb2()
 }

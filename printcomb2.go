880acc4aa8334110555ac0f36dab0492b93c4aa0
yhrouk <yhrouk@gmail.com>
2024-01-10 11:35:59 +0000

    oyyy

--- printcomb2.go
+++ printcomb2.go
@@ -1,6 +1,8 @@
 package PrintComb2
 
-import "github.com/01-edu/z01"
+import (
+	"github.com/01-edu/z01"
+)
 
 func PrintComb2() {
 	for i := '0'; i <= '9'; i++ {
@@ -12,7 +14,7 @@ func PrintComb2() {
 					z01.PrintRune(' ')
 					z01.PrintRune(k)
 					z01.PrintRune(z)
-					if i < '9' || j < '8' || k < '9' || z < '9' {
+					if i != '9' || j != '8' || k != '9' || z != '9' {
 						z01.PrintRune(',')
 						z01.PrintRune(' ')
 					}
@@ -21,4 +23,8 @@ func PrintComb2() {
 		}
 	}
 	z01.PrintRune('\n')
+}
+
+func main() {
+	PrintComb2()
 }

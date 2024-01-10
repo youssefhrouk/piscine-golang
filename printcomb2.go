c1e83a254b9b4e7e0291f646c0827cfd3c295b61
yhrouk <yhrouk@gmail.com>
2024-01-10 15:17:57 +0000

    hh

--- printcomb2.go
+++ printcomb2.go
@@ -1,5 +1,3 @@
-package piscine
-
 import "github.com/01-edu/z01"
 
 func PrintComb2() {
@@ -7,7 +5,7 @@ func PrintComb2() {
 		for b := '0'; b <= '9'; b++ {
 			for c := '0'; c <= '9'; c++ {
 				for d := '0'; d <= '9'; d++ {
-					if a < c(a == c && d > b) {
+					if a < c || (a == c && d > b) {
 
 						z01.PrintRune(a)
 						z01.PrintRune(b)

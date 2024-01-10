271613bacf763adef6528c84bc2c307cd57a5854
yhrouk <yhrouk@gmail.com>
2024-01-10 12:34:07 +0000

    ad

--- printcomb2.go
+++ printcomb2.go
@@ -7,14 +7,16 @@ func PrintComb2() {
 		for j := '0'; j <= '8'; j++ {
 			for k := '0'; k <= '9'; k++ {
 				for z := j; z <= '9'; z++ {
-					z01.PrintRune(i)
-					z01.PrintRune(j)
-					z01.PrintRune(' ')
-					z01.PrintRune(k)
-					z01.PrintRune(z)
-					if i != '9' || j != '8' || k != '9' || z != '9' {
-						z01.PrintRune(',')
+					if i < k || (i == k) && z > j {
+						z01.PrintRune(i)
+						z01.PrintRune(j)
 						z01.PrintRune(' ')
+						z01.PrintRune(k)
+						z01.PrintRune(z)
+						if i != '9' || j != '8' || k != '9' || z != '9' {
+							z01.PrintRune(',')
+							z01.PrintRune(' ')
+						}
 					}
 				}
 			}

ad04a4b1167790e5ae206e25af2b16bda577b1ea
yhrouk <yhrouk@gmail.com>
2024-01-10 20:04:24 +0000

    he

--- printnbr.go
+++ printnbr.go
@@ -2,28 +2,28 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func MyFun(nb int) {
-	c := '0'
-	if nb == 0 {
-		z01.PrintRune(c)
+func PrintNbr(n int) {
+	if n == 0 {
+		z01.PrintRune('0')
 		return
 	}
-	for i := 1; i <= nb%10; i++ {
-		c++
-	}
-	for i := -1; i >= nb%10; i-- {
-		c++
-	}
-	if nb/10 != 0 {
-		MyFun(nb / 10)
-	}
-	z01.PrintRune(c)
-	return
-}
 
-func PrintNbr(n int) {
+	if n == -9223372036854775808 {
+		z01.PrintRune('-')
+		z01.PrintRune('9')
+		PrintNbr(223372036854775808)
+		return
+	}
+
 	if n < 0 {
 		z01.PrintRune('-')
+		n = -n
 	}
-	MyFun(n)
+
+	if n < 10 {
+		z01.PrintRune(rune(n + 48))
+	} else {
+		PrintNbr(n / 10)
+		z01.PrintRune(rune(n%10 + 48))
+	}
 }

3f910d61517c2819a2ce14f65a91bfacc7dbe3be
yhrouk <yhrouk@gmail.com>
2024-01-10 17:09:34 +0000

    h

---
+++ explain.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "FirstNameOfWitness LastNameOfWitness"
+echo "123456"
+echo "Red Ferrari"
+echo "FirstNameOfSuspect1 LastNameOfSuspect1"
+echo "FirstNameOfSuspect2 LastNameOfSuspect2"
+echo "FirstNameOfSuspect3 LastNameOfSuspect3"

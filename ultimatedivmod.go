808789b5bd0425a59bcc64fa36ea2556dd432e78
yhrouk <yhrouk@gmail.com>
2024-01-11 13:26:56 +0000

    holeee

--- ultimatedivmod.go
+++ ultimatedivmod.go
@@ -1,14 +1,7 @@
 package piscine
 
-func	UltimateDivMod(a *int, b *int) {
-    nb := *a
+func UltimateDivMod(a *int, b *int) {
+	nb := *a
 	*a = *a / *b
 	*b = nb % *b
 }
-/*func main() {
-	a := 13
-	b := 2
-	UltimateDivMod(&a, &b)
-	fmt.Println(a)
-	fmt.Println(b)
-}*/

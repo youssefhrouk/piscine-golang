8e81707b3cdd42e654ecd3cabff8218ce43b14f5
yhrouk <yhrouk@gmail.com>
2024-01-11 17:59:00 +0000

    basicatoi

---
+++ basicatoi.go
@@ -0,0 +1,15 @@
+package main
+
+func BasicAtoi(s string) int {
+	cp := 0
+	for _, i := range s {
+		cp = cp*10 + int(i) - '0'
+	}
+	return cp
+}
+
+/*func main() {
+	fmt.Println(BasicAtoi("12345"))
+	fmt.Println(BasicAtoi("0000000012345"))
+	fmt.Println(BasicAtoi("000000"))
+}*/

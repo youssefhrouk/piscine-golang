5bebb8a9d95d58c0450f9da97772c8bdf9036085
yhrouk <yhrouk@gmail.com>
2024-01-13 08:05:19 +0000

    addeeed

--- sortintegertable.go
+++ sortintegertable.go
@@ -2,9 +2,9 @@ package piscine
 
 func SortIntegerTable(table []int) {
 	for i := 0; i < len(table)-1; i++ {
-		for j := 0; j < len(table)-i-1; j++ {
-			if table[j] > table[j+1] {
-				table[j], table[j+1] = table[j+1], table[j]
+		for j := len(table) - 1; j > i; j-- {
+			if table[j] > table[i] {
+				table[i], table[j] = table[j], table[i]
 			}
 		}
 	}

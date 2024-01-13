f6c1708a5f3787cc943ad25e9ef65661dab0eb86
yhrouk <yhrouk@gmail.com>
2024-01-13 08:20:28 +0000

    hellohell

--- sortintegertable.go
+++ sortintegertable.go
@@ -1,11 +1,19 @@
-package piscine
+package main
+
+import "fmt"
 
 func SortIntegerTable(table []int) {
-	for i := 0; i < len(table)-1; i++ {
-		for j := len(table) - 1; j > i; j-- {
+	for i := 0; i < len(table); i++ {
+		for j := 0; j < i; j++ {
 			if table[j] > table[i] {
 				table[i], table[j] = table[j], table[i]
 			}
 		}
 	}
+}
+
+func main() {
+	s := []int{5, 4, 3, 2, 1, 0}
+	SortIntegerTable(s)
+	fmt.Println(s)
 }

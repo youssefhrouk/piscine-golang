50ac6a836b666f15f0b8ee705f218b1e54c8ae34
yhrouk <yhrouk@gmail.com>
2024-01-15 16:07:51 +0000

    fibb

--- fibonacci.go
+++ fibonacci.go
@@ -1,18 +1,11 @@
-package main
-
-import "fmt"
+package piscine
 
 func Fibonacci(index int) int {
-	if index < 0{
+	if index < 0 {
 		return -1
 	}
 	if index < 2 {
 		return index
 	}
 	return (Fibonacci(index-1) + Fibonacci(index-2))
-}
-
-func main() {
-	arg1 := 0
-	fmt.Println(Fibonacci(arg1))
 }

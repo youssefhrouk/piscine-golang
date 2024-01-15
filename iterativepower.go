ba2de5a798b485a1097a9163eec5bfec56690c4b
yhrouk <yhrouk@gmail.com>
2024-01-15 14:21:45 +0000

    holaiter

---
+++ iterativepower.go
@@ -0,0 +1,16 @@
+package piscine
+
+func IterativePower(nb int, power int) int {
+	if power < 0 {
+		return 0
+	}
+	m := 1
+	for i := 1; i <= power; i++ {
+		m = m * nb
+	}
+	return m
+}
+
+// func main() {
+// 	fmt.Println(IterativePower(4, 2))
+// }

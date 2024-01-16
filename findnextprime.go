b02ba8a2579005a9467260bbf1b016a5dc4055af
yhrouk <yhrouk@gmail.com>
2024-01-16 11:47:55 +0000

    holaa hell

--- findnextprime.go
+++ findnextprime.go
@@ -4,7 +4,7 @@ func FindNextPrime(nb int) int {
 	if nb <= 1 {
 		return 0
 	}
-	for i := 2; i*i <= nb; i++ {
+	for i := 2; i <= nb/2; i++ {
 		if nb%i == 0 {
 			return FindNextPrime(nb + 1)
 		}
--- isprime.go
+++ isprime.go
@@ -1,15 +1,14 @@
 package piscine
 
-func IsPrime(nb int) int {
+func IsPrime(nb int) bool {
 	if nb <= 1 {
-		return 0
+		return false
 	}
-	
+	isprime := true
 	for i := 2; i <= nb/2; i++ {
 		if nb%i == 0 {
-			for k:=i ;k<i i
+			isprime = false
 		}
 	}
 	return isprime
 }
-

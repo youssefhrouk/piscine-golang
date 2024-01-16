14dc2868e26be21bdf508f12ecb5738b8c8bf635
yhrouk <yhrouk@gmail.com>
2024-01-16 17:24:11 +0000

    adding nrune

---
+++ nrune.go
@@ -0,0 +1,15 @@
+package piscine
+
+func NRune(s string, n int) rune {
+	str := []rune(s)
+	if n <= 0 || n > len(str) {
+		return 0
+	}
+	nth := ' '
+	for i := 0; i <= len(s); i++ {
+		if i == n {
+			nth = str[i-1]
+		}
+	}
+	return nth
+}

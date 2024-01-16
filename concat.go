2abe7cb8206c46159fd34dd45c8c8172142d0424
yhrouk <yhrouk@gmail.com>
2024-01-16 19:21:29 +0000

    add

--- concat.go
+++ concat.go
@@ -1,13 +1,5 @@
-package main
-
-import (
-	"fmt"
-)
+package piscine
 
 func Concat(str1 string, str2 string) string {
 	return str1 + str2
-}
-
-func main() {
-	fmt.Println(Concat("Hello!", " How are you?"))
 }

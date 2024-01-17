905eb96e15fa8ae83af66c0134562757ddddbbbf
yhrouk <yhrouk@gmail.com>
2024-01-17 09:09:02 +0000

    upperHell

---
+++ toupper.go
@@ -0,0 +1,13 @@
+package piscine
+
+func ToUpper(s string) string {
+	str := " "
+	for _, v := range s {
+		if v >= 97 && v <= 122 {
+			str += string(v - 32)
+		} else {
+			str += string(v)
+		}
+	}
+	return str
+}

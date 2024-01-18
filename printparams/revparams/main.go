63aca545017741be1456c735538db855f55fff41
yhrouk <yhrouk@gmail.com>
2024-01-18 14:57:25 +0000

    hell

--- printparams/revparams/main.go
+++ printparams/revparams/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	arg := os.Args
-	for i := len(arg) - 1; i <= 1; i-- {
+	for i := len(arg) - 1; i >= 1; i-- {
 		slice := []rune(arg[i])
 		for _, j := range slice {
 			z01.PrintRune(j)

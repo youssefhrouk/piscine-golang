51723d36426af4c55866dbd53d1445dc159b5b27
yhrouk <yhrouk@gmail.com>
2024-01-19 07:30:46 +0000

    add

--- join.go
+++ join.go
@@ -1,20 +1,14 @@
-package main
+package piscine
 
 import "fmt"
 
 func Join(strs []string, sep string) string {
-		str:=""
- for i:= range strs{
-	str += strs[i]
-	if i<len(strs)-1{
-		str+=sep
+	str := ""
+	for i := range strs {
+		str += strs[i]
+		if i < len(strs)-1 {
+			str += sep
+		}
 	}
- }
- return str
+	return str
 }
-
-func main() {
-	toConcat := []string{"Hello!", " How", " are", " you?"}
-	fmt.Println(Join(toConcat, ":"))
-}
-

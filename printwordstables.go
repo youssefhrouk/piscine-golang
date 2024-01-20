0c704ad2e330d6605a1c944714c29792d197f678
yhrouk <yhrouk@mail.com>
2024-01-20 17:58:39 +0000

    heey

--- printwordstables.go
+++ printwordstables.go
@@ -3,9 +3,9 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintWordsTables(a []string) {
-	for _, v1 := range a {
-		for _, v2 := range v1 {
-			z01.PrintRune(v2)
+	for _, s1 := range a {
+		for _, s2 := range s1 {
+			z01.PrintRune(s2)
 		}
 		z01.PrintRune('\n')
 	}

8eafb8f0dd52aa186d9195ec09827d97371a14d1
yhrouk <yhrouk@gmail.com>
2024-01-22 14:52:34 +0000

    hee

--- split.go
+++ split.go
@@ -1,24 +1,5 @@
 package piscine
 
-
-func isWhitespace(c byte) bool {
-	return c == ' ' || c == '\t' || c == '\n'
-}
-
-func SplitWhiteSpaces(s string) []string {
-	var result []string
-	index := 0
-
-	for i := 0; i <= len(s); i++ {
-		if i == len(s) || isWhitespace(s[i]) {
-			if i > index {
-				result = append(result, s[index:i])
-			}
-			index = i + 1
-		}
-	}
-	return result
-}
 func Split(s, sep string) []string {
 	a := 0
 	for i := range sep {

3426a60063ddeb16f33f96015a81025481ea69b5
yhrouk <yhrouk@gmail.com>
2024-01-24 15:39:43 +0100

    holl

---
+++ collatzcountdown.go
@@ -0,0 +1,18 @@
+package piscine
+
+func CollatzCountdown(start int) int {
+	if start <= 0 {
+		return -1
+	}
+	count := 0
+	for start > 1 {
+		if start%2 == 0 {
+			start = start / 2
+		} else if start%2 != 0 {
+			start = (start * 3) + 1
+		}
+		count++
+
+	}
+	return count
+}

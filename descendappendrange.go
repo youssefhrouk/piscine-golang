3284d4dd9392f0be2ea7ab4e67278212c44f3279
yhrouk <yhrouk@gmail.com>
2024-01-25 00:52:51 +0100

    hh

--- descendappendrange.go
+++ descendappendrange.go
@@ -1,12 +1,12 @@
 package piscine
 
 func DescendAppendRange(max, min int) []int {
-	var slice []int
-	if max <= min {
-		return slice
+	if min >= max {
+		return []int{}
 	}
+	var r []int
 	for i := max; i > min; i-- {
-		slice = append(slice, i)
+		r = append(r, i)
 	}
-	return slice
+	return r
 }

64203b7f9ba2eb8f6ee8a698b18967676e9f89d8
yhrouk <yhrouk@gmail.com>
2024-01-24 19:34:00 +0100

    hgelle

--- dealapackofcards.go
+++ dealapackofcards.go
@@ -1,17 +1,31 @@
 package piscine
 
-import (
-	"fmt"
-)
+import "github.com/01-edu/z01"
 
 func DealAPackOfCards(deck []int) {
-	numP := 4
-	cp := len(deck) / numP
+	numPlayers := len(deck) / 3 // Number of players based on deck size
 
-	for i := 0; i < numP; i++ {
+	for i := 0; i < numPlayers; i++ {
+		z01.PrintRune('P')
+		z01.PrintRune('l')
+		z01.PrintRune('a')
+		z01.PrintRune('y')
+		z01.PrintRune('e')
+		z01.PrintRune('r')
+		z01.PrintRune(' ')
+		z01.PrintRune(rune(49 + i)) // Printz01. player number
+		z01.PrintRune(':')
+		z01.PrintRune(' ')
 
-		playerC := deck[i*cp : (i+1)*cp]
+		// Print player's cards
+		for j := 0; j < 3; j++ {
+			z01.PrintRune(rune(deck[i*3+j] + 48)) // Print card value as ASCII digit
+			if j < 2 {
+				z01.PrintRune(',')
+				z01.PrintRune(' ')
+			}
+		}
 
-		fmt.Printf("Player %d: %v$\n", i+1, playerC)
+		z01.PrintRune('\n')
 	}
 }

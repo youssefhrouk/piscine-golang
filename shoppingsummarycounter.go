3ff843065fd563e04d4b0fb6ab7e24d31bb7392c
yhrouk <yhrouk@gmail.com>
2024-01-24 17:58:12 +0100

    holl

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -1,16 +1,38 @@
-package piscine
+package main
+
+func toLower(char byte) byte {
+	if 'A' <= char && char <= 'Z' {
+		return char + ('a' - 'A')
+	}
+	return char
+}
 
 func ShoppingSummaryCounter(str string) map[string]int {
-	myMap := map[string]int{
-		"Burger": 2,
-		"Water":  4,
-		"Carrot": 4,
-		"Coffee": 1,
-		"Chips":  1,
+	var wordStart int
+	summary := make(map[string]int)
+
+	for i := 0; i < len(str); i++ {
+		if str[i] == ' ' || str[i] == '.' || str[i] == ',' {
+			if wordStart < i {
+				word := str[wordStart:i]
+				wordLower := ""
+				for j := 0; j < len(word); j++ {
+					wordLower += string(toLower(word[j]))
+				}
+				summary[wordLower]++
+			}
+			wordStart = i + 1
+		}
 	}
 
-	for i, v := range myMap {
-		myMap[i] = v
+	if wordStart < len(str) {
+		word := str[wordStart:]
+		wordLower := ""
+		for j := 0; j < len(word); j++ {
+			wordLower += string(toLower(word[j]))
+		}
+		summary[wordLower]++
 	}
-	return myMap
+
+	return summary
 }

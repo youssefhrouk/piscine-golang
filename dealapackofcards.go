9276dbb4c9adccdfbe5bcdbb126680d12421f45a
yhrouk <yhrouk@gmail.com>
2024-01-24 19:50:48 +0100

    hol

--- dealapackofcards.go
+++ dealapackofcards.go
@@ -1,31 +1,10 @@
 package piscine
 
-import "github.com/01-edu/z01"
+import "fmt"
 
 func DealAPackOfCards(deck []int) {
-	numPlayers := len(deck) / 3 // Number of players based on deck size
-
-	for i := 0; i < numPlayers; i++ {
-		z01.PrintRune('P')
-		z01.PrintRune('l')
-		z01.PrintRune('a')
-		z01.PrintRune('y')
-		z01.PrintRune('e')
-		z01.PrintRune('r')
-		z01.PrintRune(' ')
-		z01.PrintRune(rune(49 + i)) // Printz01. player number
-		z01.PrintRune(':')
-		z01.PrintRune(' ')
-
-		// Print player's cards
-		for j := 0; j < 3; j++ {
-			z01.PrintRune(rune(deck[i*3+j] + 48)) // Print card value as ASCII digit
-			if j < 2 {
-				z01.PrintRune(',')
-				z01.PrintRune(' ')
-			}
-		}
-
-		z01.PrintRune('\n')
-	}
+	fmt.Printf("Player 1: %v, %v, %v\n", deck[0], deck[1], deck[2])
+	fmt.Printf("Player 2: %v, %v, %v\n", deck[3], deck[4], deck[5])
+	fmt.Printf("Player 3: %v, %v, %v\n", deck[6], deck[7], deck[8])
+	fmt.Printf("Player 4: %v, %v, %v\n", deck[9], deck[10], deck[11])
 }

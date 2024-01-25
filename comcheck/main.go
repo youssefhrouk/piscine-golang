a07c3bdec48fdfa44ae42460aaf50e10e0bf29f5
Your Name <you@example.com>
2024-01-25 02:25:10 +0100

    hh

---
+++ comcheck/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+	for _, v := range args {
+		if v == "01" || v == "galaxy" || v == "galaxy 01" {
+			fmt.Println("Alert!!!")
+			break
+		}
+	}
+}

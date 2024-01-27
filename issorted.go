a4f3218f97332ca7a0fa01ecf7f4b7f01b75be90
yhrouk <yhrouk@gmail.com>
2024-01-27 10:47:12 +0100

    jhhhg

--- issorted.go
+++ issorted.go
@@ -1,10 +1,22 @@
 package piscine
 
-func IsSorted(f func(a, b int) int, a []int) bool {
-	for i := range a {
-		if f(a[i-1], a[i]) > 0 {
-			return false
+func IsSorted(f func(a, b int) int, tab []int) bool {
+	if len(tab) > 1 {
+		if f(tab[0], tab[1]) >= 0 {
+			for i := 0; i < len(tab)-1; i++ {
+				if f(tab[i], tab[i+1]) < 0 {
+					return false
+				}
+			}
+		}
+		if f(tab[0], tab[1]) <= 0 {
+			for i := 0; i < len(tab)-1; i++ {
+				if f(tab[i], tab[i+1]) > 0 {
+					return false
+				}
+			}
 		}
 	}
+
 	return true
 }

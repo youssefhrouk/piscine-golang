6ee56cd2e6142d77d3ba3fd709e3d471f0d27352
yhrouk <yhrouk@gmail.com>
2024-01-28 16:52:58 +0100

    hh

--- listpushfront.go
+++ listpushfront.go
@@ -1,14 +1,4 @@
-package main
-
-type NodeL struct {
-	Data interface{}
-	Next *NodeL
-}
-
-type List struct {
-	Head *NodeL
-	Tail *NodeL
-}
+package piscine
 
 func ListPushFront(l *List, data interface{}) {
 	newnode := &NodeL{Data: data, Next: nil}

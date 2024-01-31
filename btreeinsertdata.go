0667b7a5c333b30a17725309905480b37f5687a0
yhrouk <yhrouk@gmail.com>
2024-01-31 10:05:03 +0100

    hh

--- btreeinsertdata.go
+++ btreeinsertdata.go
@@ -11,8 +11,10 @@ func BTreeInsertData(root *TreeNode, data string) *TreeNode {
 	}
 	if data < root.Data {
 		root.Left = BTreeInsertData(root.Left, data)
+		root.Left.Parent = root
 	} else {
 		root.Right = BTreeInsertData(root.Right, data)
+		root.Right.Parent = root
 	}
 	return root
 }

245f4a14db56862bbbcfc34ce1cf5f46fe2f1015
Christopher  Fremond <dev@01-edu.org>
2024-02-01 19:23:44 +0000

    Update 'btreedeletenode.go'

--- btreedeletenode.go
+++ btreedeletenode.go
@@ -38,3 +38,44 @@ func BTreeMin(root *TreeNode) *TreeNode {
 	}
 	return BTreeMin(root.Left)
 }
+
+func BTreeInsertData(root *TreeNode, data string) *TreeNode {
+	if root == nil {
+		return &TreeNode{Data: data}
+	}
+	if data < root.Data {
+		root.Left = BTreeInsertData(root.Left, data)
+		root.Left.Parent = root
+	} else {
+		root.Right = BTreeInsertData(root.Right, data)
+		root.Right.Parent = root
+	}
+	return root
+}
+
+func BTreeIsBinary(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	if root.Left != nil && root.Left.Data > root.Data {
+		return false
+	}
+	if root.Right != nil && root.Right.Data < root.Data {
+		return false
+	}
+	if !BTreeIsBinary(root.Left) || !BTreeIsBinary(root.Right) {
+		return false
+	}
+
+	return true
+}
+
+func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
+	if root == nil || root.Data == elem {
+		return root
+	}
+	if elem < root.Data {
+		return BTreeSearchItem(root.Left, elem)
+	}
+	return BTreeSearchItem(root.Right, elem)
+}
